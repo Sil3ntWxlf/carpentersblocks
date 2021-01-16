68cb71b10e45eea9451eb773f78c0ddc515c7763
Sil3ntWxlf <sil3ntwxlf@gmail.com>
2021-01-16 04:30:01 -0800

    A lot of refactoring and setup

---
+++ src/main/java/com/carpentersblocksreborn/DataGeneration.java
@@ -0,0 +1,19 @@
+package com.carpentersblocksreborn;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGeneration {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        if (event.includeServer()) {
+            generator.addProvider(new Recipes(generator));
+            generator.addProvider(new LootTables(generator));
+        }
+
+    }
+}
---
+++ src/main/java/com/carpentersblocksreborn/LootTables.java
@@ -0,0 +1,27 @@
+package com.carpentersblocksreborn;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.LootTableProvider;
+import net.minecraft.loot.LootParameterSet;
+import net.minecraft.loot.LootTable;
+import net.minecraft.util.ResourceLocation;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+class LootTables extends LootTableProvider {
+
+    public LootTables(DataGenerator generatorIn) {
+        super(generatorIn);
+    }
+
+    @Override
+    protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> getTables() {
+        ImmutableList.Builder<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> lootTables = ImmutableList.builder();
+        lootTables.add();
+        return lootTables.build();
+    }
+}
