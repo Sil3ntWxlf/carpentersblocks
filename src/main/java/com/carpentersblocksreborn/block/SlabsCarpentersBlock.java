dfdcd1d852560001c3ebaf359341030376b6b5b7
Sil3ntWxlf <sil3ntwxlf@gmail.com>
2021-01-18 01:41:07 -0800

    Added Carpenters Slabs changed license type in mods.toml

---
+++ src/main/java/com/carpentersblocksreborn/block/SlabsCarpentersBlock.java
@@ -0,0 +1,10 @@
+package com.carpentersblocksreborn.block;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SlabBlock;
+
+public class SlabsCarpentersBlock extends SlabBlock {
+    public SlabsCarpentersBlock(BlockState defaultState, Properties properties) {
+        super(properties);
+    }
+}
